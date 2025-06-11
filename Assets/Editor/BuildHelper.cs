cd05ae6d8802462f6f68db11ff14392e73965d66
YangLingYun <825067524@qq.com>
2025-06-12 02:34:55 +0800

    补充MissingMetadataChecker注释

--- Assets/Editor/BuildHelper.cs
+++ Assets/Editor/BuildHelper.cs
@@ -35,16 +35,20 @@ public class BuildHelper
 
         return names.ToArray();
     }
-    
+
+    /// <summary>
+    /// 一般来说，发布热更新包时，由于中间可能调用过generate/all，SettingsUtil.GetAssembliesPostIl2CppStripDir(target)目录中包含了最新的aot dll，
+    /// 肯定无法检查出类型或者函数裁剪的问题。
+    /// 需要在构建完主包后，将当时的aot dll保存下来，供后面补充元数据或者裁剪检查。
+    ///  换句话说,验证一定要在打包之前,或者Generate/AotDlls之前执行
+    /// </summary>
+    /// <returns></returns>
     [MenuItem("HybridTool/验证元数据是否需要补充")]
     public static bool CheckAccessMissingMetadata()
     {
         BuildTarget target = EditorUserBuildSettings.activeBuildTarget;
-        // aotDir指向 构建主包时生成的裁剪aot dll目录，而不是最新的SettingsUtil.GetAssembliesPostIl2CppStripDir(target)目录。
-        // 一般来说，发布热更新包时，由于中间可能调用过generate/all，SettingsUtil.GetAssembliesPostIl2CppStripDir(target)目录中包含了最新的aot dll，
-        // 肯定无法检查出类型或者函数裁剪的问题。
-        // 需要在构建完主包后，将当时的aot dll保存下来，供后面补充元数据或者裁剪检查。
-        //  换句话说,验证一定要在打包之前,或者Generate/AotDlls之前执行
+
+
         string aotDir = SettingsUtil.GetAssembliesPostIl2CppStripDir(target);
 
         // 第2个参数hotUpdateAssNames为热更新程序集列表。对于旗舰版本，该列表需要包含DHE程序集，即SettingsUtil.HotUpdateAndDHEAssemblyNamesIncludePreserved。
